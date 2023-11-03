2b4d322218e5a95d418d3c71624f4c91586a5b19
qaize <ikhwani.syahbana15@gmail.com>
2023-11-03 17:29:49 +0700

    search masih bug

--- resources/views/quran/home.blade.php
+++ resources/views/quran/home.blade.php
@@ -7,7 +7,9 @@
   <button id="search-button">
     <i class="fa-solid fa-magnifying-glass"></i>
   </button>
+  <div class="suggestion"></div>
 </div>
+
 @endsection
 
 @section('surah')
@@ -16,6 +18,10 @@
 
 @section('content')
     <div id="main-body" class="main-body"></div>
+    <div id="loading-screen">
+        <div class="loader"></div>
+        <p>Loading...</p>
+      </div>
     <div id="pagination" class="pagination">
       <button id="prev" class="buttons">Prev</button>
       <button id="next" class="buttons">Next</button>
