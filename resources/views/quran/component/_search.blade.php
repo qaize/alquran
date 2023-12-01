e86744da3d64839257164b12c610561b66a6cdaf
qaize <ikhwani.syahbana15@gmail.com>
2023-12-01 11:16:17 +0700

    fix routes

--- resources/views/quran/component/_search.blade.php
+++ resources/views/quran/component/_search.blade.php
@@ -1,4 +1,4 @@
-<div class="banner"><a href="/home"><h1><strong>A</strong>l <strong>Q</strong>ur'an</h1></a></div>
+<div class="banner"><a href="/"><h1><strong>A</strong>l <strong>Q</strong>ur'an</h1></a></div>
 
 <div id="search">
     <input id="search-input" type="text" placeholder="Cari berdasarkan Surah, Nomor Surah, Arti Surah" />
